daf9b3f3957ea4cce83c29aef1c8823e15177e0c
remi-picard <remi-picard@users.noreply.github.com>
2025-09-01 14:53:50 +0000

    deploy: c0670c06bb9805cf1085ed7f6e0af037bd59ac5d

---
+++ assets/slidev/27-DBOaIJyE.js
@@ -0,0 +1,12 @@
+import{_ as g}from"./CodeBlockWrapper-D7z4zp7h.js";import{I as k}from"../default-CczNVIyl.js";import{b as v,ah as p}from"../index-CFK1ULHe.js";import{p as m,u,f as x}from"./context-DrS8VniP.js";import{b as $,o as y,w as d,g as e,e as P,m as h,a0 as n,v as b,x as w}from"../modules/vue-DCvFXe3w.js";import"../modules/unplugin-icons-CSIP7O7s.js";import"../modules/shiki-C5MGpnk0.js";const A={__name:"27",setup(c,{expose:s}){s(),m(p);const{$slidev:a,$nav:t,$clicksContext:l,$clicks:o,$page:r,$renderContext:f,$frontmatter:_}=u(),i={$slidev:a,$nav:t,$clicksContext:l,$clicks:o,$page:r,$renderContext:f,$frontmatter:_,InjectedLayout:k,get frontmatter(){return p},get useSlideContext(){return u},get _provideFrontmatter(){return m},get _frontmatterToProps(){return x}};return Object.defineProperty(i,"__isScriptSetup",{enumerable:!1,value:!0}),i}};function C(c,s,a,t,l,o){const r=g;return y(),$(t.InjectedLayout,b(w(t._frontmatterToProps(t.frontmatter,26))),{default:d(()=>[s[1]||(s[1]=e("h2",null,"Keyword",-1)),s[2]||(s[2]=e("h3",null,"Syntaxe Robot Framework",-1)),P(r,h({},{ranges:["1","1-3","1,5-7","1,9-12"]}),{default:d(()=>[...s[0]||(s[0]=[e("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[e("code",{class:"language-text"},[e("span",{class:"line"},[e("span",null,"*** Keywords ***")]),n(`
+`),e("span",{class:"line"},[e("span",null,"Mon Premier Keyword")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    Log    Hello World")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    ")]),n(`
+`),e("span",{class:"line"},[e("span",null,"Mon Premier Keyword Avec Argument")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    [Arguments]    ${name}")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    Log    Hello ${name}")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    ")]),n(`
+`),e("span",{class:"line"},[e("span",null,"Mon Premier Keyword Avec Argument Et Return")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    [Arguments]    ${name}")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    Log    Hello ${name}")]),n(`
+`),e("span",{class:"line"},[e("span",null,"    RETURN    42")])])],-1)])]),_:1},16)]),_:1},16)}const j=v(A,[["render",C],["__file","/@slidev/slides/27.md"]]);export{j as default};
