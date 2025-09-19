e187858bb4ecd05c46a0b478972723c463c97513
PICARD Rémi <picard.remi@gmail.com>
2025-09-19 17:31:56 +0200

    Ajoute le logo robot qui tourne et avance à chaque changement de slide

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="slidev-layout default">
     <slot />
+    <RobotProgress />
     <footer>
       <div id="title" class="ml-1"><img src="/images/Robot-framework-logo.png" class="logo" /><span>Développer avec Robot Framework</span></div>
       <div id="page" class="mr-1">{{ $page }}</div>
@@ -8,4 +9,5 @@
   </div>
 </template>
 <script setup lang="ts">
+import RobotProgress from '../components/RobotProgress.vue'
 </script>
